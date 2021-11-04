d487491307cd495ea1161a32a9257e86486739b4
Anastasiiall <a.p.chentsova@student.khai.edu>
2021-11-04 11:23:00 +0300

    ChatBot 2-nd stage

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -1,2 +1,7 @@
 print("Hello! My name is Chat_bot.")
 print("I was created in 2021.")
+
+
+print("Please, remind me tour name.")
+str_name = str(input())
+print("What a great name you have," + str_name +"!")
