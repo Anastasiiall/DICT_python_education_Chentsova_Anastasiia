8283d5f5f6102e90702c51f4623c2325a94a3e05
Anastasiiall <a.p.chentsova@student.khai.edu>
2021-11-04 11:28:31 +0300

    ChatBot 4-th stage

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -5,3 +5,12 @@ b = int(input())
 c = int(input())
 int_age = (a*70+b*21+c*15)%105
 print("Your age is " + str(int_age) + "that's a good time to start programming!")
+
+
+print("Now I will prove to you that I count to any number you want.")
+str_num = int(input())
+x = int()
+while x <= str_num:
+print("" + str(x) + "!")
+x += 1
+print("Completed, have a nice day!")
