1b22728571ea3d561a72e43a51b3318bdcc99392
Anastasiiall <a.p.chentsova@student.khai.edu>
2021-11-04 11:25:45 +0300

    ChatBot 3-rd stage

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -1,7 +1,7 @@
-print("Hello! My name is Chat_bot.")
-print("I was created in 2021.")
-
-
-print("Please, remind me tour name.")
-str_name = str(input())
-print("What a great name you have," + str_name +"!")
+print("Let me guess your age.")
+print("Enter remainders of diving your age by 3,5 and 7.")
+a = int(input())
+b = int(input())
+c = int(input())
+int_age = (a*70+b*21+c*15)%105
+print("Your age is " + str(int_age) + "that's a good time to start programming!")
