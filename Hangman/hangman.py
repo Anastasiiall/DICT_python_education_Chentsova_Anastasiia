b66e652e793b87005754e887f24da8bd54b1e5a4
Anastasiiall <a.p.chentsova@student.khai.edu>
2021-11-11 12:53:55 +0300

    stage 2

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,7 +1,15 @@
 print("HANGMAN")
 print("The game will be available soon.")
 
-
-
+words_list = ['python,java']
+secret = random.choice(words_list)
+print("Guess the word:")
+def Hangman(words):
+    words = str(input())
+    if str(input()) in secret
+    print("You survived!")
+else:
+    if str(input()) not in secret
+    print("You lost!")
 
 
