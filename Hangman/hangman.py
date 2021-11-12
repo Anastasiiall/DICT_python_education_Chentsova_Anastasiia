90f9c7e6c98aa13eecfde5c744b85ce378486082
Anastasiiall <a.p.chentsova@student.khai.edu>
2021-11-12 16:13:09 +0300

    stage 2

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,27 +1,24 @@
+import random
 print("HANGMAN")
 print("The game will be available soon.")
 
-words_list = ['python,java']
-secret = random.choice(words_list)
-print("Guess the word:")
-def Hangman(words):
-    words = str(input())
-    if str(input()) in secret
+word_list = ['python', 'java', 'javascript', 'php']
+secrets = random.choice(word_list)
+x = input("Guess the word:")
+print(x)
+if x in secrets:
     print("You survived!")
 else:
-    if str(input()) not in secret
     print("You lost!")
 
 
-word_list = ['python', 'java', 'javascript', 'php']
 
-import random
+# secret = random.choice(word_list)
+# str_word = str(input())
+# guesse = input("Guess the word:" + str_word)
+# if guesse in secret:
+#     print("You survived!")
+# else:
+#     if guesse not in secret:
+#         print("You lost!")
 
-secret = random.choice(word_list)
-str_word = str(input())
-guesse = input("Guess the word:" + str_word)
-if guesse in secret:
-    print("You survived!")
-else:
-    if guesse not in secret:
-        print("You lost!")
