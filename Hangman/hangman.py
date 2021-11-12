3d91793937c47cb8d45de2e8e319a039db2f10a2
Anastasiiall <a.p.chentsova@student.khai.edu>
2021-11-12 16:46:01 +0300

    stage 4

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -3,32 +3,35 @@ print("HANGMAN")
 print("The game will be available soon.")
 
 
-word = ["python"]
-x = input("Guess the word:")
-print(x)
-if x in word:
-    print("You survived!")
-else:
-    print("You lost!")
+# word = ["python"]
+# x = input("Guess the word:")
+# print(x)
+# if x in word:
+#     print("You survived!")
+# else:
+#     print("You lost!")
+#
+#
+# word_list = ['python', 'java', 'javascript', 'php']
+# secrets = random.choice(word_list)
+# x = input("Guess the word:")
+# print(x)
+# if x in secrets:
+#     print("You survived!")
+# else:
+#     print("You lost!")
 
 
 word_list = ['python', 'java', 'javascript', 'php']
 secrets = random.choice(word_list)
+
+len_secrets = len(secrets[2:-1])
+print(secrets[:3] + '-'*len_secrets)
 x = input("Guess the word:")
 print(x)
-if x in secrets:
-    print("You survived!")
+if x == secrets:
+    print("You WON")
 else:
     print("You lost!")
 
-
-
-# secret = random.choice(word_list)
-# str_word = str(input())
-# guesse = input("Guess the word:" + str_word)
-# if guesse in secret:
-#     print("You survived!")
-# else:
-#     if guesse not in secret:
-#         print("You lost!")
 
