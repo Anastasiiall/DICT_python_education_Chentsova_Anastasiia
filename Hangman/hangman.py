8d1de03a37d93ab54ce477d71427039bac90ba04
Anastasiiall <a.p.chentsova@student.khai.edu>
2021-11-28 10:54:25 +0300

    stage 5

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,37 +1,57 @@
 import random
-print("HANGMAN")
-print("The game will be available soon.")
 
 
-# word = ["python"]
-# x = input("Guess the word:")
-# print(x)
-# if x in word:
-#     print("You survived!")
-# else:
-#     print("You lost!")
-#
-#
-# word_list = ['python', 'java', 'javascript', 'php']
-# secrets = random.choice(word_list)
-# x = input("Guess the word:")
-# print(x)
-# if x in secrets:
-#     print("You survived!")
-# else:
-#     print("You lost!")
+def start_game():
+    print("HANGMAN\nThe game will be available soon.")
+    t = 0
+    word_list = ['python', 'java', 'javascript', 'php']
+    secrets = (random.choice(word_list))
+    word = '_' * len(secrets)
+    while t <= 7:
+        a = input("" + word + "\nInput a letter:")
+        if a in secrets:
+            print("That letter appear in the word")
+            v = ''
+            for i in range(len(secrets)):
+                if a == secrets[i]:
+                    v += a
+                else:
+                    v += word[i]
+                if v == secrets:
+                    print("Your word is", secrets, "\nYou win!\nThanks for playing!")
+                    quit()
+                if a in word[i]:
+                    print("You have already entered this letter")
+                    t -= 1
+            word = v
+        else:
+            print("That letter doesn't appear in the word" + "\nNumber of mistakes:" + str(t) + " out of 7")
+        t += 1
+        y = a
+        if y.isalpha():
+            print()
+        else:
+            print("Please enter a lowercase English letter")
+            t -= 1
+        k = len(a)
+        if k == 1:
+            print()
+        else:
+            print("You should input a single letter")
+            t -= 1
+        if t == 8:
+            print("Your word is", secrets, "\nYou lose!\nThanks for playing!")
+        break
 
 
-word_list = ['python', 'java', 'javascript', 'php']
-secrets = random.choice(word_list)
-
-len_secrets = len(secrets[2:-1])
-print(secrets[:3] + '-'*len_secrets)
-x = input("Guess the word:")
-print(x)
-if x == secrets:
-    print("You WON")
-else:
-    print("You lost!")
+def game_1():
+    n = input('Type "play" to play the game, "exit" to quit:')
+    if n == "play":
+        start_game()
+    elif n == "exit":
+        return game_1()
+    else:
+        return game_1()
 
 
+game_1()
