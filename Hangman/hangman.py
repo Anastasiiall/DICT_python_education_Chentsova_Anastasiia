87b4e824cb168bc9f46dd0546098018086e4839e
Anastasiiall <a.p.chentsova@student.khai.edu>
2021-12-02 13:27:18 +0300

    stage 8

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -6,34 +6,34 @@ def start_game():
     t = 0
     word_list = ['python', 'java', 'javascript', 'php']
     secrets = (random.choice(word_list))
-    line = '_' * len(secrets)
-    word = input("" + line + "\nInput a letter:")
+    k = '_' * len(secrets)
     while t <= 7:
-        if word in secrets:
-            print("That letter appear in the word")
-            n = ''
+        letter = input("" + k + "\nInput a letter:")
+        if letter in secrets:
+            print('That letter appear in the word')
+            b = ''
             for i in range(len(secrets)):
-                if word == secrets[i]:
-                    n += word
+                if letter == secrets[i]:
+                    b += letter
                 else:
-                    n += line[i]
-                if n == secrets:
+                    b += k[i]
+                if b == secrets:
                     print("Your word is", secrets, "\nYou win!\nThanks for playing!")
                     quit()
-                if word in line[i]:
+                if letter in k[i]:
                     print("You have already entered this letter")
                     t -= 1
-            line = n
+            k = b
         else:
-            print("That letter doesn't appear in the word" + "\nNumber of mistakes:" + str(t) + " out of 7")
+            print("That letter doesn't appear in the word, used moves " + str(t+1) + " out of 8")
         t += 1
-        y = word
+        y = letter
         if y.isalpha():
             print()
         else:
             print("Please enter a lowercase English letter")
             t -= 1
-        o = len(word)
+        o = len(letter)
         if o == 1:
             print()
         else:
@@ -41,14 +41,14 @@ def start_game():
             t -= 1
         if t == 8:
             print("Your word is", secrets, "\nYou lose!\nThanks for playing!")
-        break
+            break
 
 
 def game_1():
-    k = input('Type "play" to play the game, "exit" to quit:')
-    if k == "play":
+    n = input('Type "play" to play the game, "exit" to quit:')
+    if n == "play":
         start_game()
-    elif k == "exit":
+    elif n == "exit":
         return game_1()
     else:
         return game_1()
